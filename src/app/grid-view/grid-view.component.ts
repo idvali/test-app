ee39981732e3aa5bfc65c7c88305a0d356786faa
omedi <idvali@omedia.dev>
2022-11-20 16:35:33 +0400

    Add list view

---
+++ src/app/grid-view/grid-view.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-grid-view',
+  templateUrl: './grid-view.component.html',
+  styleUrls: ['./grid-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GridViewComponent{
+  boxes$ = this.apiService.getBoxData().valueChanges;
+
+  constructor(private apiService: ApiService) { }
+}
