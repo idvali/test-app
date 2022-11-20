d713fc88f6e87c1b07d33b75802096741acfebb8
omedi <idvali@omedia.dev>
2022-11-20 18:38:09 +0400

    Update balance

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { noop } from 'rxjs';
 import { ApiService } from '../api.service';
 import { Wallet } from '../types';
 
@@ -8,10 +9,14 @@ import { Wallet } from '../types';
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   userData$ = this.apiService.getUserData().valueChanges;
 
   constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.subscribeToWallet().subscribe(noop)
+  }
 
   calculateBalance(wallets: Wallet[]): number {
     let balance = 0;
